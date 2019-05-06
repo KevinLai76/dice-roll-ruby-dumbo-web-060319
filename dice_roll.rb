e75fd965fdace5a6dc4230b84b7847b89ecfe11c
Kevin <kevinlai76@gmail.com>
2019-05-06 21:42:37 +0000

    Automatically backed up by Learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,9 +3,9 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll(num1, num2)
-  
-  return rand(#{num1}..#{num2})
- 
+  result = []
+  result << rand("#{num1}..#{num2}")
+  return result.to_i
 end
 
 roll(1,6)
